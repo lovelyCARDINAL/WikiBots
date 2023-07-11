77c19ad628d7b8548d7ad6e9d20058bda6d7d4ef
星海 <yan@musinya.com>
2023-07-12 00:04:30 +0800

    feat: Change config

--- src/actiontest.js
+++ src/actiontest.js
@@ -1,7 +1,7 @@
 import { MediaWikiApi } from 'wiki-saikou';
 import config from './utils/config.js';
 
-const api = new MediaWikiApi(config.api.zh, {
+const api = new MediaWikiApi(config.zh.api, {
 	headers: {
 		'api-user-agent': config.apiuseragent || '',
 	},
@@ -9,7 +9,7 @@ const api = new MediaWikiApi(config.api.zh, {
 
 console.log(`Start time: ${new Date().toISOString()}`);
 
-api.login(config.bot.zh.name, config.bot.zh.password)
+api.login(config.zh.bot.name, config.zh.bot.password)
 	.then(console.log, console.error)
 	.then(() => {
 		return api.postWithToken('csrf', {
