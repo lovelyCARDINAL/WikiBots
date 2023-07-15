d5373df9e661a9be2b4508fe1b15edf13857fe0c
星海 <yan@musinya.com>
2023-07-15 12:08:17 +0800

    feat: create brokenRedirectDelete.js

---
+++ src/brokenRedirectDelete.js
@@ -0,0 +1,33 @@
+import { MediaWikiApi } from 'wiki-saikou';
+import config from './utils/config.js';
+
+const api = new MediaWikiApi(config.zh.api, { headers: { 'api-user-agent': config.apiuseragent || '' } });
+
+console.log(`Start time: ${new Date().toISOString()}`);
+
+api.login(config.zh.abot.name, config.zh.abot.password)
+	.then(console.log, console.error)
+	.then(async() => {
+		const { data :{ query: { querypage: { results } } } } = await api.post({
+			list: 'querypage',
+			qppage: 'BrokenRedirects',
+			qplimit: 'max',
+		});
+		if (results.length) {
+			await Promise.all(results.map(async (item) => {
+				if (item.ns === 2 || item.title % 2 === 1) {
+					const { data } = await api.postWithToken('csrf', {
+						action: 'delete',
+						title: item.title,
+						reason: '受损重定向',
+						tags: 'Bot',
+						watchlist: 'nochange',
+					});
+					console.log(JSON.stringify(data));
+				}
+			}));
+		} else {
+			console.log('No broken redirect.');
+		}
+		console.log(`End time: ${new Date().toISOString()}`);
+	});
