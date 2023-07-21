7c258d5b539e4210ef65a424209f20aa85ddf341
星海 <yan@musinya.com>
2023-07-21 21:35:05 +0800

    chore

--- src/utils/readData.js
+++ src/utils/readData.js
@@ -4,12 +4,13 @@ import { fileURLToPath } from 'url';
 
 /**
  * @param {string} name
+ * @returns {Promise<string>}
  */
 function readData(name) {
 	const __filename = fileURLToPath(import.meta.url);
 	const __dirname = path.dirname(__filename);
 	const filePath = path.join(__dirname, `../../data/${name}`);
-  
+	
 	return new Promise((resolve, reject) => {
 		fs.readFile(filePath, 'utf8', (error, data) => {
 			if (error) {
