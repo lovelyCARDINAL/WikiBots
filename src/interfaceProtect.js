58a2bb7d4eb7aa4c7a708b055eaa6bff99707c53
星海 <yan@musinya.com>
2023-08-05 01:26:13 +0800

    fix

--- src/interfaceProtect.js
+++ src/interfaceProtect.js
@@ -1,5 +1,5 @@
 import { MediaWikiApi } from 'wiki-saikou';
-import config from './utils/localConfig.js';
+import config from './utils/config.js';
 
 const SITE_LIST = ['zh', 'cm'];
 const api = {};
