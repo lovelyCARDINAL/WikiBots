224395709750d4b503742768d53afc09dc6e603c
星海 <yan@musinya.com>
2023-08-06 03:11:54 +0800

    fix

--- src/addCategoryRedirect.js
+++ src/addCategoryRedirect.js
@@ -5,60 +5,62 @@ const SITE_LIST = ['zh', 'cm'];
 
 (async () => {
 	console.log(`Start time: ${new Date().toISOString()}`);
-	await Promise.all(
-		SITE_LIST.map(async (site) => {
-			const api = new MediaWikiApi(config[site].api, { headers: { 'api-user-agent': config.apiuseragent } });
-			await api.login(config[site].bot.name, config[site].bot.password).then(console.log);
+	await Promise.all(SITE_LIST.map(async (site) => {
+		const api = new MediaWikiApi(config[site].api, { headers: { 'api-user-agent': config.apiuseragent } });
+		await api.login(config[site].bot.name, config[site].bot.password).then(console.log);
 
-			const { data: { query: { allredirects, categorymembers } } } = await api.post({
-				list: 'allredirects|categorymembers',
-				cmtitle: 'Category:已重定向的分类',
-				cmprop: 'ids|title',
-				cmnamespace: '14',
-				cmlimit: 'max',
-				arprop: 'title|ids',
-				arnamespace: '14',
-				arlimit: 'max',
-			});
+		const { data: { query: { allredirects, categorymembers } } } = await api.post({
+			list: 'allredirects|categorymembers',
+			cmtitle: 'Category:已重定向的分类',
+			cmprop: 'ids|title',
+			cmnamespace: '14',
+			cmlimit: 'max',
+			arprop: 'title|ids',
+			arnamespace: '14',
+			arlimit: 'max',
+		});
 
-			const pageids = allredirects
-				.map(({ fromid }) => fromid)
-				.filter((fromid) => !categorymembers
-					.map(({ pageid }) => pageid)
-					.some((pageid) => pageid === fromid));
+		const pageids = allredirects
+			.map(({ fromid }) => fromid)
+			.filter((fromid) => !categorymembers
+				.map(({ pageid }) => pageid)
+				.some((pageid) => pageid === fromid));
+		
+		if (!pageids.length) {
+			return;
+		}
 
-			const { data: { query: { pages } } } = await api.post({
-				prop: 'redirects',
-				pageids,
-				redirects: true,
-				rdprop: 'pageid|title',
-				rdlimit: 'max',
-			});
+		const { data: { query: { pages } } } = await api.post({
+			prop: 'redirects',
+			pageids,
+			redirects: true,
+			rdprop: 'pageid|title',
+			rdlimit: 'max',
+		});
 
-			const result = pages.map((item) => item?.redirects
-				.filter((redirect) => redirect.ns === 14 && pageids.includes(redirect.pageid))
-				.flatMap((redirect) => [redirect.pageid, item.title]),
-			).filter((arr) => arr.length);
+		const result = pages.map((item) => item?.redirects
+			.filter((redirect) => redirect.ns === 14 && pageids.includes(redirect.pageid))
+			.flatMap((redirect) => [redirect.pageid, item.title]),
+		).filter((arr) => arr.length);
 			
-			await Promise.all(
-				result.map(async ([pageid, target]) => {
-					await api.postWithToken('csrf', {
-						action: 'edit',
-						pageid,
-						appendtext: `\n{{Cr|${target.replace('Category:', '')}}}`,
-						minor: true,
-						bot: true,
-						nocreate: true,
-						tags: 'Bot',
-						summary: `添加至「[[:${target}]]」的[[Template:分类重定向|分类重定向]]`,
-						watchlist: 'nochange',
-					}, {
-						retry: 10,
-						noCache: true,
-					}).then(({ data }) => console.log(JSON.stringify(data)));
-				}),
-			);
-		}),
-	);
+		await Promise.all(
+			result.map(async ([pageid, target]) => {
+				await api.postWithToken('csrf', {
+					action: 'edit',
+					pageid,
+					appendtext: `\n{{Cr|${target.replace('Category:', '')}}}`,
+					minor: true,
+					bot: true,
+					nocreate: true,
+					tags: 'Bot',
+					summary: `添加至「[[:${target}]]」的[[Template:分类重定向|分类重定向]]`,
+					watchlist: 'nochange',
+				}, {
+					retry: 10,
+					noCache: true,
+				}).then(({ data }) => console.log(JSON.stringify(data)));
+			}),
+		);
+	}));
 	console.log(`End time: ${new Date().toISOString()}`);
 })();
