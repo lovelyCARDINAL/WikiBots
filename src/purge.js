06066a024df3246b7d825d36a9b60f61168882b5
星海 <yan@musinya.com>
2023-08-12 02:37:58 +0800

    feat: create purge.js

---
+++ src/purge.js
@@ -0,0 +1,38 @@
+import { MediaWikiApi } from 'wiki-saikou';
+import config from './utils/config.js';
+
+const api = new MediaWikiApi(config.zh.api, { headers: { 'api-user-agent': config.apiuseragent } });
+
+(async () => {
+	console.log(`Start time: ${new Date().toISOString()}`);
+	await api.login(config.zh.abot.name, config.zh.abot.password).then(console.log);
+
+	const { data: { query: { pages: [{ revisions: [{ content }] }] } } } = await api.post({
+		prop: 'revisions',
+		titles: 'User:星海子/BotConfig/purge.json',
+		rvprop: 'content',
+	});
+	const setting = JSON.parse(content || '{}');
+
+	await Promise.all([
+		Promise.all(setting.group.map(async ({ prefix, ns }) => {
+			await api.post({
+				action: 'purge',
+				generator: 'allpages',
+				gapprefix: prefix,
+				gapnamespace: ns,
+				gaplimit: 'max',
+			}, {
+				retry: 10,
+			}).then(({ data }) => console.log(JSON.stringify(data)));
+		})),
+		api.post({
+			action: 'purge',
+			titles: setting.page,
+		}, {
+			retry: 10,
+		}).then(({ data }) => console.log(JSON.stringify(data))),
+	]);
+
+	console.log(`End time: ${new Date().toISOString()}`);
+})();
