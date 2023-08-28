46402891ed43cbb4b23eeb7a7f1d8cc3d1393db7
星海 <yan@musinya.com>
2023-08-29 02:25:23 +0800

    feat: create removeBlockedUserrights.js

---
+++ src/removeBlockedUserrights.js
@@ -0,0 +1,39 @@
+import moment from 'moment';
+import { MediaWikiApi } from 'wiki-saikou';
+import config from './utils/config.js';
+
+const api = new MediaWikiApi(config.zh.api, {
+	headers: { 'api-user-agent': config.apiuseragent },
+});
+
+(async () => {
+	console.log(`Start time: ${new Date().toISOString()}`);
+	await api.login(config.zh.abot.name, config.zh.abot.password).then(console.log);
+
+	const { data: { query: { allusers } } } = await api.post({
+		list: 'allusers',
+		augroup: 'manually-confirmed|extendedconfirmed',
+		auprop: 'blockinfo',
+		aulimit: 'max',
+	});
+
+	const time = moment().subtract(1, 'week');
+	const userids = allusers
+		.filter(({ blockexpiry, blockedtimestamp }) => blockexpiry === 'infinity' && moment(blockedtimestamp).isBefore(time))
+		.map(({ userid }) => userid);
+
+	await Promise.all(userids.map(async (userid) => {
+		await api.postWithToken('userrights', {
+			action: 'userrights',
+			userid,
+			remove: 'goodeditor|honoredmaintainer|techeditor|manually-confirmed|file-maintainer|extendedconfirmed',
+			reason: '无限期封禁',
+			tags: 'Bot',
+		}, {
+			retry: 30,
+			noCache: true,
+		}).then(({ data }) => console.log(JSON.stringify(data)));
+	}));
+
+	console.log(`End time: ${new Date().toISOString()}`);
+})();
