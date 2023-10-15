ee45b557d9ef1d53f7310efb4a8a3de26f945796
星海 <yan@musinya.com>
2023-10-15 18:42:52 +0800

    feat: 生成组活跃数据

--- src/checkLeaderActivities.js
+++ src/checkLeaderActivities.js
@@ -1,4 +1,5 @@
 import { MediaWikiApi } from 'wiki-saikou';
+import Parser from 'wikiparser-node';
 import config from './utils/config.js';
 
 const zhapi = new MediaWikiApi(config.zh.api, {
@@ -75,14 +76,15 @@ async function updateData(text) {
 		).then(console.log),
 	]);
 
-	const userlist = await (async () => {
-		const { data: { parse: { wikitext } } } = await zhapi.post({
-			action: 'parse',
-			pageid: '490849',
-			prop: 'wikitext',
-		}, {
-			retry: 15,
-		});
+	const { data: { parse: { wikitext } } } = await zhapi.post({
+		action: 'parse',
+		pageid: '490849',
+		prop: 'wikitext',
+	}, {
+		retry: 15,
+	});
+
+	const userlist = (() => {
 		const regex = /{{User\|(.+?)}}/gi;
 		const data = Array.from(wikitext.matchAll(regex), (match) => match[1]?.replace(/^\w/, (first) => first.toUpperCase()));
 		return [...new Set(data)].sort();
@@ -93,19 +95,40 @@ async function updateData(text) {
 		queryContribs(cmapi, userlist, '0|10|14|12|4|6'),
 	]).then((result) => result.flat());
 
-	let text = '* 本页面为[[U:星海-interfacebot|机器人]]生成的编辑组负责人90日内中文萌娘百科与萌娘共享主、模板、分类、帮助、萌娘百科、文件名字空间下编辑数统计。\n* 生成时间：{{subst:#time:Y年n月j日 (D) H:i (T)}}｜{{subst:#time:Y年n月j日 (D) H:i (T)|||1}}\n<div style="display: flex; flex-wrap: wrap; justify-content: center;">\n<div style="min-width: 60%; margin:0 3rem 1rem">\n{| class="wikitable sortable" width=100%\n|-\n! 用户名 !! 编辑数\n';
-
+	const userContribsCount = {};
 	for (const user of userlist) {
 		const contribsCount = data.filter((item) => item.user === user).length || 0;
-		const count = contribsCount
-			? contribsCount >= 3
-				? `data-sort-value="${contribsCount}"|${contribsCount}次`
-				: `data-sort-value="${contribsCount}"|<span style="color:red">${contribsCount}次</span>`
-			: 'data-sort-value="0"|<i style="color:red">无相关编辑</i>';
-		text += `|-\n| ${userInfo(user)} || ${count}\n`;
+		userContribsCount[user] = contribsCount;
 	}
 
-	text += '|}\n</div>\n</div>\n\n[[Category:萌娘百科数据报告]]';
+	const parser = Parser.parse(wikitext);
+	const table = parser.querySelector('table');
+	const rowCount = table.getRowCount();
+	let text = '* 本页面为[[U:星海-interfacebot|机器人]]生成的编辑组负责人90日内中文萌娘百科与萌娘共享主、模板、分类、帮助、萌娘百科、文件名字空间下编辑数统计。\n* 生成时间：{{subst:#time:Y年n月j日 (D) H:i (T)}}｜{{subst:#time:Y年n月j日 (D) H:i (T)|||1}}\n<div style="display: flex; flex-wrap: wrap; justify-content: center;">\n<div style="min-width: 60%; margin:0 3rem 1rem">\n{| class="wikitable sortable" width=100%\n|-\n! width=35%|编辑组 !! width=35%|用户名 !! 编辑数 !! 是否活跃';
+
+	for (let i = 1; i < rowCount; i++) {
+		const groupCell = String(table.getNthCell({ row: i, column: 0 })).trim();
+		const users = (() => {
+			const userCell = String(table.getNthCell({ row: i, column: 2 }));
+			const regex = /{{User\|(.+?)}}/gi;
+			return Array.from(userCell.matchAll(regex), (match) => match[1]?.replace(/^\w/, (first) => first.toUpperCase()));
+		})();
+		const userCount = users.length;
+		const isActive = Math.max(...users.map((user) => userContribsCount[user])) >= 3
+			? '是'
+			: '<b style="color:red">否</b>';
+		function userContribs(user) {
+			const contribsCount = userContribsCount[user];
+			return contribsCount >= 3
+				? `data-sort-value="${contribsCount}"|${contribsCount}次`
+				: `data-sort-value="${contribsCount}"|<span style="color:red">${contribsCount}次</span>`;
+		}
+		text += userCount === 1 
+			? `\n|-\n${groupCell} || ${userInfo(users[0])} || ${userContribs(users[0])} || ${isActive}`
+			: `\n|-\n| rowspan=${userCount}${groupCell} || ${userInfo(users[0])} || ${userContribs(users.shift())} || rowspan=${userCount} |${isActive}${users.map((user) => `\n|-\n| ${userInfo(user)} || ${userContribs(user)}`).join('')}`;
+	}
+
+	text += '\n|-\n|}\n</div>\n</div>\n\n[[Category:萌娘百科数据报告]]';
 	
 	await updateData(text);
 	
