a9dbad6473416a9655621aca78274341edffdbd0
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2023-11-28 02:45:21 +0800

    feat: Update markAbuserStatus.js with wikiparser-node (#53)
    * Update markAbuserStatus.js with wikiparser-node
    
    * fix selector in markAbuserStatus.js
    
    * fix typo in markAbuserStatus.js
    
    * fix const declaration in markAbuserStatus.js
    
    * chore: eslint
    
    * fix: username
    
    * chore: Update package.json
    
    * revert "chore: Update package.json"
    
    * Update markAbuserStatus.js
    
    * refactor markAbuserStatus.js
    
    * Update markAbuserStatus.js
    
    ---------
    
    Co-authored-by: 星海 <yan@musinya.com>

--- src/Blocked/markAbuserStatus.js
+++ src/Blocked/markAbuserStatus.js
@@ -1,5 +1,8 @@
 import { MediaWikiApi } from 'wiki-saikou';
+import Parser from 'wikiparser-node';
 import config from '../utils/config.js';
+
+Parser.config = 'moegirl';
 
 const api = new MediaWikiApi(config.zh.api, {
 	headers: { 'user-agent': config.useragent },
@@ -23,26 +26,29 @@ const api = new MediaWikiApi(config.zh.api, {
 		retry: 25,
 	});
 
-	const lines = content.split('\n');
-	const lineRegex = /^\* *(?:<span.*?>|-\{)?\[\[User:.*?(?<!{{No[ _]abuselog}}\s*)$/i,
-		userRegex = /\[\[User:(.*?)\]\]/;
+	const root = Parser.parse(content, false, 10); // 不解析语言变体转换
+	const selector = 'list + link[name^=User:], list + html#span + link[name^=User:]';
+	const users = root.querySelectorAll(selector);
+	const linesWithTemplate = new Set(root.querySelectorAll('template#Template:No_abuselog').map((token) => token.getBoundingClientRect().top));
 	const time = new Date(Date.now() - 90 * 24 * 60 * 60 * 1000).toISOString();
 
-	await Promise.all(lines.map(async (line, i) => {
-		if (lineRegex.test(line)) {
-			const user = line.match(userRegex)[1];
+	await Promise.all(users.map(async (user) => {
+		const rect = user.getBoundingClientRect();
+		const name = user.name.slice(5);
+		const endLine = rect.top + rect.height - 1;
+		if (!linesWithTemplate.has(endLine)) {
 			const { data: { query: { abuselog, usercontribs } } } = await api.post({
 				list: 'abuselog|usercontribs',
-				afluser: user,
+				afluser: name,
 				afllimit: '1',
 				uclimit: '1',
 				ucend: time,
-				ucuser: user,
+				ucuser: name,
 			}, {
 				retry: 25,
 			});
 			if (!abuselog.length && !usercontribs.length) {
-				lines[i] += ' {{No abuselog}}';
+				user.after(' {{No abuselog}}');
 			}
 		}
 	}));
@@ -50,7 +56,7 @@ const api = new MediaWikiApi(config.zh.api, {
 	await api.postWithToken('csrf', {
 		action: 'edit',
 		title: '萌娘百科:长期破坏者',
-		text: lines.join('\n'),
+		text: String(root),
 		bot: true,
 		nocreate: true,
 		tags: 'Bot',
