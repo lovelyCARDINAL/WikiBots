b2215c97c57a9d4e9f68af5f5688a0a2f4487b9d
Bhsd <2545473905@qq.com>
2025-09-28 12:00:23 +0800

    Update groupLeader.js

--- src/Activity/groupLeader.js
+++ src/Activity/groupLeader.js
@@ -104,6 +104,7 @@ async function updateData(text) {
 	}
 
 	const parser = Parser.parse(wikitext);
+	/** @type {Parser.TableToken} */
 	const table = parser.querySelector('table');
 	const rowCount = table.getRowCount();
 	let text = '* 本页面为[[U:星海-interfacebot|机器人]]生成的编辑组负责人90日内中文萌娘百科与萌娘共享主、模板、分类、帮助、萌娘百科、文件名字空间下编辑数统计。\n* 生成时间：{{subst:#time:Y年n月j日 (D) H:i (T)}}｜{{subst:#time:Y年n月j日 (D) H:i (T)|||1}}\n<div style="display: flex; flex-wrap: wrap; justify-content: center;">\n<div style="min-width: 60%; margin:0 3rem 1rem">\n{| class="wikitable sortable" width=100%\n|-\n! width=35%|编辑组 !! width=35%|用户名 !! 编辑数 !! 是否活跃';
