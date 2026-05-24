7b586a8465bd1874a49d73325e150013a623ac30
星海 <yan@musinya.com>
2026-05-24 15:53:14 +0800

    Revert "test"
    This reverts commit 8f389acab3fb0d6b654d742d9f31669af638acab.

--- src/Personal/test.js
+++ src/Personal/test.js
@@ -3,36 +3,28 @@ import config from '../utils/config.js';
 
 const api = new MediaWikiApi({
 	baseURL: config.zh.api,
-	fexiosConfig: {
-		headers: { 'user-agent': config.useragent },
-	},
 });
 
 (async () => {
-	try {
-		const loginResult = await api.login(
-			config.zh.bot.name,
-			config.zh.bot.password,
-			undefined,
-			{ retry: 25, noCache: true },
-		);
-		console.log(loginResult);
+	console.log(`Start time: ${new Date().toISOString()}`);
+	
+	await api.login(
+		config.zh.bot.name,
+		config.zh.bot.password,
+		undefined,
+		{ retry: 25, noCache: true },
+	).then(console.log);
 
-		const editResult = await api.postWithToken('csrf', {
-			action: 'edit',
-			title: 'User:星海子/test/001',
-			text: `${new Date().toISOString()}`,
-			summary: 'Test',
-			bot: true,
-			minor: true,
-			tags: 'Bot',
-		}, {
-			retry: 50,
-			noCache: true,
-		});
-		console.log(JSON.stringify(editResult.data));
-
-	} catch (error) {
-		console.error(error);
-	}
+	await api.postWithToken('csrf', {
+		action: 'edit',
+		title: 'User:星海子/test/001',
+		text: `${new Date().toISOString()}`,
+		summary: 'Github Actions Test',
+		bot: true,
+		minor: true,
+		tags: 'Bot',
+	}, {
+		retry: 50,
+		noCache: true,
+	}).then(({ data }) => console.log(JSON.stringify(data)));
 })();
